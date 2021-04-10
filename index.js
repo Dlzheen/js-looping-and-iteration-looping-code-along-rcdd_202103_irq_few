cd764f0152fcf6f0a7aa39616edf1038469b0ce4
Dlzheen Mahmood <dlzheenmahmood@gmail.com>
2021-04-10 07:59:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solutions in this file
 
-Function writeCards(cards, event){
+function writeCards(cards, event){
   let finalEvent = [];
   let eventName ;
   for (let i=0; i<cards.length; i++)
