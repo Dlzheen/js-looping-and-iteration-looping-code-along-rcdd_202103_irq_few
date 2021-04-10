e55a5032eb181b5f26e312782a1a6ccda263806f
Dlzheen Mahmood <dlzheenmahmood@gmail.com>
2021-04-10 07:41:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // Code your solutions in this file
-const names=["Ada", "Brendan", "Ali"];
-const event = "birthday";
-Function writeCards(names,event){
+
+Function writeCards(["Ada", "Brendan", "Ali"],"birthday"){
   for (let i=0, i<names.length ,i++)
   {
     return `Thank you,${names[i]}, for the ${event} gift`;
